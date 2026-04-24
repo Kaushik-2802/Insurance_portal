8c5719f53ada19862b4553769789ac00ba711627
A <2488244@cognizant.com>
2026-04-24 14:40:22 +0530

    buy-insurence page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
+import BuyInsurance from "./pages/BuyInsurance"
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/buy-insurance" element={<BuyInsurance />} />
       </Routes>
     </BrowserRouter>
   )
